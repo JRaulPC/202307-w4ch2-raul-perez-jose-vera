4186426fdf1a20a6c05f216f894f879780b7f659
Jacint Blanch <jacintblanch5@gmail.com>
2023-08-01 22:17:52 +0200

    Get character data from api

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,20 @@
-const App = () => {
-  return <div></div>;
+import { useEffect, useState } from "react";
+import CharactersData from "../../types";
+
+const App = (): React.ReactElement => {
+  const [characters, setCharacters] = useState<CharactersData[]>([]);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        "https://starwars-characters-api-dsef.onrender.com/characters",
+      );
+      const characterResponse = (await response.json()) as CharactersData[];
+      setCharacters([...characterResponse]);
+      characters[0];
+    })();
+  }, [characters]);
+
+  return <div className="app"></div>;
 };
 
 export default App;
--- src/types.tsx
+++ src/types.tsx
@@ -1,7 +1,9 @@
-export interface Characters {
+interface CharactersData {
   name: string;
   img: string;
   height: string;
   mass: string;
   created: string;
 }
+
+export default CharactersData;
