b1ab60b3bfbdd5b17ab86dfc2565e03b92eeb136
Jacint Blanch <jacintblanch5@gmail.com>
2023-08-02 20:42:58 +0200

    Logic increase button

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -1,4 +1,6 @@
+import React, { useState } from "react";
 import CharacterData from "../../types";
+import Button from "../ButtonComponent/ButtonComponent";
 import "./CharacterCard.css";
 
 interface CharacterCardProps {
@@ -8,6 +10,21 @@ interface CharacterCardProps {
 const CharacterCard = ({
   character: { id, name, height, mass, created },
 }: CharacterCardProps): React.ReactElement => {
+  const [number, setNumber] = useState(0);
+  const incrementNumber = () => {
+    if (number === 500) {
+      return;
+    }
+    setNumber((number: number) => number + 1);
+  };
+
+  const decrementNumber = () => {
+    if (number === 0) {
+      return;
+    }
+    setNumber((number: number) => number - 1);
+  };
+
   return (
     <article className="character">
       <h2 className="character-name">{name}</h2>
@@ -18,6 +35,10 @@ const CharacterCard = ({
       <ul className="character-atributes">
         <li>Altura: {height} Cm</li>
         <li>Peso: {mass} Kg</li>
+        <div className="container">
+          <Button text="+" actionOnClick={incrementNumber} />
+          <Button text="-" actionOnClick={decrementNumber} />
+        </div>
         <li>It was created on {created.slice(0, 10)}</li>
       </ul>
     </article>
